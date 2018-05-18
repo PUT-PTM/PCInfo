89cbccb9bd0d3bb18025d989f9c78ae910dbb051
vasudanin <37503240+vasudanin@users.noreply.github.com>
2018-05-18 21:54:37 +0200

    Add files via upload

--- SerialPortWPF/WpfApp1/MainWindow.xaml.cs
+++ SerialPortWPF/WpfApp1/MainWindow.xaml.cs
@@ -30,16 +30,27 @@ namespace WpfApp1
         UpdateVisitor updateVisitor = new UpdateVisitor(); 
         Computer computer = new Computer();
 
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
             foreach (string s in SerialPort.GetPortNames())
             {
                 nrPortu.Items.Add(s);
             }
         }
-
-        public class UpdateVisitor : IVisitor
+        private static void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            MessageBox.Show(indata);
+        }
+    public class UpdateVisitor : IVisitor
         {
             public void VisitComputer(IComputer computer)
             {
@@ -149,7 +160,8 @@ namespace WpfApp1
                 }
                 }
                 _serialPort.Write(data);
-            MessageBox.Show(data);
+            Thread.Sleep(10);
+                MessageBox.Show(_serialPort.ReadLine());
                 computer.Close();
             
         }
