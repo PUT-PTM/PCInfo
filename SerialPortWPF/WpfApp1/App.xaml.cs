f5d85a797148403371741892a281d18e66947ba8
vasudanin <37503240+vasudanin@users.noreply.github.com>
2018-05-22 17:55:38 +0200

    Add files via upload

--- SerialPortWPF/WpfApp1/App.xaml.cs
+++ SerialPortWPF/WpfApp1/App.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -16,22 +15,3 @@ namespace WpfApp1
     {
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace WpfApp1
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-}
->>>>>>> 0c6c489906e7ed67509d3869afe5d4ada0a2021e
